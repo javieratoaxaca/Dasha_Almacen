8c059bd11f6ff8b8ff5c1a73b9e33bee78cf1e11
javieratoaxaca <1277_oax@1277-oax.conacional.net>
2026-01-07 15:10:06 -0600

    Iniciando Proyecto

---
+++ Dasha_Almancen.Presentacion/Frm_Articulos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dasha_Almancen.Presentacion
+{
+    public partial class Frm_Articulos : Form
+    {
+        public Frm_Articulos()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Articulos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
