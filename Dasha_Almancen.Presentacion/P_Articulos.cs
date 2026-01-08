b3333340d05d91a1174d2843fa4ca32193344154
javieratoaxaca <javier.atoaxaca@gmail.com>
2026-01-08 16:37:06 -0600

    Se creo la Clase de Articulos la cual tiene las Propiedades para la insercion de datos a la BD

---
+++ Dasha_Almancen.Presentacion/P_Articulos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dasha_Almancen.Presentacion
+{
+    public class P_Articulos
+    {
+        public int Codigo_Ar {  get; set; }
+        public string Descricpion_Ar { get; set; }
+        public string Marca_Ar {  get; set; }
+        public int Codigo_Um {  get; set; }
+        public int Codigo_Ca {  get; set; }
+        public decimal Stock_Actual {  get; set; }
+        public string Fecha_crea { get; set; }
+        public string Fecha_Modifica { get; set; }
+    }
+}
